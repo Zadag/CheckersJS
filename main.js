8e612167fe6452cce817d27b7b3647b4151761b7
rudy ordoyne <rudolfordoyne@protonmail.com>
2021-06-11 18:26:40 -0400

    add generator for board element

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log("test");
+
+function gridGenerate() {
+  const board = document.getElementById("board");
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      const square = document.createElement("div");
+      square.id = `${i}x${j}`;
+      square.classList.add("square");
+      if ((i + j) % 2 === 0) {
+        square.classList.add("red");
+      } else {
+        square.classList.add("black");
+      }
+      board.append(square);
+    }
+  }
+}
+
+gridGenerate();
