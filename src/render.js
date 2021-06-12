990b5ec15bc4325a282553c729e405d85973d928
Sana Khalid <sanabibi.khalid@gmail.com>
2021-06-12 16:12:29 -0700

    Update board colors/add player circles

--- src/render.js
+++ src/render.js
@@ -3,14 +3,26 @@ export default function gridGenerate() {
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
       const square = document.createElement('div');
+      const circle = document.createElement('div');
       square.id = `${i}x${j}`;
       square.classList.add('square');
       if ((i + j) % 2 === 0) {
-        square.classList.add('red');
+        square.classList.add('cream');
       } else {
-        square.classList.add('black');
+        square.classList.add('tan');
       }
       board.append(square);
+
+
+      if (((i + j) % 2 === 0) && (i < 3)) {
+        circle.classList.add('red');
+      };
+
+      if (((i + j) % 2 === 1) && (i > 4)) {
+        circle.classList.add('black');
+      };
+
+      square.append(circle);
     }
   }
 }
