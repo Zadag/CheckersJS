0080a78d38f13cd9740d24215b6dbf0af4424857
T-McVee <iam@tmcvee.com>
2021-06-18 16:51:18 -0700

    Update initialize method & remove clearSquare method: initialize() now returns null for empty squares and calls markerFactory for occupied squares.

--- src/gameboard.js
+++ src/gameboard.js
@@ -6,16 +6,14 @@ const Gameboard = ((markerFactory) => {
       const row = [];
 
       for (let j = 0; j < 8; j++) {
-        const square = { hasMarker: false };
+        let square = null;
 
         if (((i + j) % 2 === 1) && (i < 3)) {
-          square.hasMarker = true;
-          square.marker = markerFactory('red');
+          square = markerFactory(true);
         }
 
         if (((i + j) % 2 === 1) && (i > 4)) {
-          square.hasMarker = true;
-          square.marker = markerFactory('black');
+          square = markerFactory();
         }
 
         row.push(square);
@@ -34,36 +32,26 @@ const Gameboard = ((markerFactory) => {
     return board[x][y];
   };
 
-  const clearSquare = (index) => {
-    const [x, y] = index;
-    // const htmlField = document.getElementById(`${x}x${y}`);
-    // htmlfield.removeChild(htmlfield.childNodes[0]);
-    board[x][y].hasMarker = false;
-    delete board[x][y].marker;
-  };
-
   const updateSquare = (index, marker) => {
     // checks board at given index, updates to given marker,
     //  if marker empty set square to empty ;
     const [x, y] = index;
 
-    if (board[x][y].hasMarker && marker) {
+    if (board[x][y] !== null && marker) {
       console.log('square occupied');
       return;
     }
 
     // const htmlfield = document.getElementById(`${x}x${y}`);
 
-    if (!board[x][y].hasMarker && marker) {
-      board[x][y].hasMarker = true;
-      board[x][y].marker = marker;
+    if (board[x][y] == null && marker) {
+      board[x][y] = marker;
       // htmlfield.childNodes[0] = marker;
       return;
     }
 
     if (!marker) {
-      board[x][y].hasMarker = false;
-      delete board[x][y].marker;
+      board[x][y] = null;
       // htmlfield.removeChild(htmlfield.childNodes[0]);
     }
   };
@@ -75,10 +63,8 @@ const Gameboard = ((markerFactory) => {
   };
 
   return {
-    board, // remove board from public after development
     getSquare,
     updateSquare,
-    clearSquare,
     reset,
   };
 });
