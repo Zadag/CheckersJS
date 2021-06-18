dd112705be66fbaade1e64b36739f759dd892b11
T-McVee <iam@tmcvee.com>
2021-06-17 22:29:45 -0700

    update gameboard instance to call markerFactory as inputs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import gridGenerate from './render';
 import Gameboard from './gameboard';
+import MarkerFactory from './markerFactory';
 
 gridGenerate();
 
-// Replace inputs with marker factory once defined
-const gameboard = Gameboard({ color: 'red', isKing: false }, { color: 'black', isKing: false });
+const gameboard = Gameboard(MarkerFactory('red'), MarkerFactory('black'));
