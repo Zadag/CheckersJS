ecd1b3c32292a79edd74274d898b71237c89e338
T-McVee <iam@tmcvee.com>
2021-06-26 22:27:12 -0700

    Begin defining the getAvailableMoves method

--- src/gameController.js
+++ src/gameController.js
@@ -1,25 +1,42 @@
 const gameController = ((playerFactory, gameboard, display) => {
   const playerRed = playerFactory(true);
   const playerBlack = playerFactory(false);
-  let redPlayersTurn;
+  let redPlayersTurn = true;
 
   function getPlayer(redPlayer) {
     return redPlayer ? playerRed : playerBlack;
   }
 
+  const getAvailableMoves = (index) => {
+    // return an array containing an index for each possible move.
+    // if no moves are available return null
+    const [x, y] = index;
+    const { isRed, isKing } = gameboard.getSquare(index);
+
+    const direction = isRed ? 1 : -1;
+
+    if (x + direction <= 7 && x + direction >= 0) {
+      gameboard.getSquare([x + direction, y])
+    }
+  };
+
   const takeTurn = () => {
     // Set player
     redPlayersTurn = !redPlayersTurn;
+    console.log(`Begin ${redPlayersTurn ? 'Red' : 'Black'} player's turn`);
 
-    // Check active player's markers for available moves
+    // get the location of current player's markers
+    const markerLocations = gameboard.getMarkerLocations(redPlayersTurn);
 
-    gameboard.forEach((square) => {
-      // Check if square has marker
-      if (square != null && square.isRed === redPlayersTurn) {
-        console.log(square);
-      }
+    // Check current player's markers for available moves
+    markerLocations.forEach((index) => {
+      getAvailableMoves(index);
     });
+
+    // TBD
   };
+
+  takeTurn();
 });
 
 export default gameController;
